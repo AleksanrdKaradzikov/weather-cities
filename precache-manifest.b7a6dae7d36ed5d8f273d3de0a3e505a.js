12c8b7d816204d568acd8479ebd92d686da7d75e
AleksandrKaradzikov <karadzikovaleksandr@gmail.com>
2019-12-23 16:00:11 +0300

    Updates

---
+++ precache-manifest.b7a6dae7d36ed5d8f273d3de0a3e505a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c3c65175e5127fbe3a07e800604e762",
+    "url": "/weather-cities/index.html"
+  },
+  {
+    "revision": "9c7a5ca871b67cb9786d",
+    "url": "/weather-cities/static/css/2.667739f9.chunk.css"
+  },
+  {
+    "revision": "2131355bc7154aee3c0f",
+    "url": "/weather-cities/static/css/main.e7e4594c.chunk.css"
+  },
+  {
+    "revision": "9c7a5ca871b67cb9786d",
+    "url": "/weather-cities/static/js/2.673fb234.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather-cities/static/js/2.673fb234.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2131355bc7154aee3c0f",
+    "url": "/weather-cities/static/js/main.3a2cfb0d.chunk.js"
+  },
+  {
+    "revision": "79f1c3eabf3c72aeb335",
+    "url": "/weather-cities/static/js/runtime-main.fd4c2bd0.js"
+  },
+  {
+    "revision": "cc54c19b29890841267ee0750eb25a00",
+    "url": "/weather-cities/static/media/polygon.cc54c19b.jpg"
+  }
+]);
